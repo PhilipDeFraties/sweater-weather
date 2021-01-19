78fc2a2e5813519050014be09e4f1d24d7df6c2f
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-18 18:05:13 -0700

    adds sad path conditional to search action in forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,8 +2,14 @@ module Api
   module V1
     class ForecastController < ApplicationController
       def search
-        forecast = ForecastFacade.get_forecast(params[:location])
-        render json: ForecastSerializer.new(forecast)
+        unless params[:location].empty?
+          forecast = ForecastFacade.get_forecast(params[:location])
+          render json: ForecastSerializer.new(forecast)
+        else
+          render json: { error: 'missing parameter',
+                         message: "Location parameter with value required in search request" },
+            status: 400
+        end
       end
     end
   end
