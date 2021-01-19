b52f527f8fdef54fa3a782bbfde9d5068569f53d
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 11:33:41 -0700

    changes time param in food method in busincess service

--- app/services/business_service.rb
+++ app/services/business_service.rb
@@ -7,7 +7,7 @@ class BusinessService
     response = conn.get('/v3/businesses/search') do |faraday|
       faraday.params['term'] = 'restaurants'
       faraday.params['location'] = location
-      faraday.params['open_at'] = time
+      faraday.params['open_at'] = (time + Time.now.to_i)
       faraday.params['categories'] = type
       faraday.headers["Authorization"] = 'Bearer 4PUZ7qouz08KV23ls8BV-ZRnzKDiHkl3a4fxMMCddyAlgG8ef5X5RbJ8mdQDWAbeGNmr1qKvXlBGKkhMZo0DqV74b-6Pt-L5auLMgp9Qpes6S00PvjjJg0836-gFYHYx'
     end
