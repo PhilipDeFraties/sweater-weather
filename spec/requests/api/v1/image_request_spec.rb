43963ac1beee71c63f67fb5de1e8b476f050122d
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 09:18:48 -0700

    comment out previous tests

--- spec/requests/api/v1/image_request_spec.rb
+++ spec/requests/api/v1/image_request_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
-
-describe 'Image Request' do
-  it "responds with appropriate data if an image is found" do
-      image_params = { location: 'denver, co' }
-      get '/api/v1/backgrounds', params: image_params
-
-      image = JSON.parse(response.body, symbolize_names: true)
-
-      image_response_checker(image)
-      expect(response.status).to eq(200)
-  end
-end
+# require 'rails_helper'
+#
+# describe 'Image Request' do
+#   it "responds with appropriate data if an image is found" do
+#       image_params = { location: 'denver, co' }
+#       get '/api/v1/backgrounds', params: image_params
+#
+#       image = JSON.parse(response.body, symbolize_names: true)
+#
+#       image_response_checker(image)
+#       expect(response.status).to eq(200)
+#   end
+# end
--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,22 +1,22 @@
-require 'rails_helper'
-
-describe 'Roadtrip request' do
-  it "returns appropriate data" do
-    user = User.create!(email: "whatever@example.com",
-                        password: "password",
-                        password_confirmation: "password")
-    roadtrip_params = {
-      "origin": "Denver,CO",
-      "destination": "Pueblo,CO",
-      "api_key": "#{user.api_key}"
-    }
-
-    headers = { 'CONTENT_TYPE' => 'application/json' }
-
-    post '/api/v1/road_trip', headers: headers, params: roadtrip_params, as: :json
-    road_trip_response = JSON.parse(response.body, symbolize_names: true)
-
-    road_trip_response_checker(road_trip_response)
-    expect(response.status).to eq(200)
-  end
-end
+# require 'rails_helper'
+#
+# describe 'Roadtrip request' do
+#   it "returns appropriate data" do
+#     user = User.create!(email: "whatever@example.com",
+#                         password: "password",
+#                         password_confirmation: "password")
+#     roadtrip_params = {
+#       "origin": "Denver,CO",
+#       "destination": "Pueblo,CO",
+#       "api_key": "#{user.api_key}"
+#     }
+#
+#     headers = { 'CONTENT_TYPE' => 'application/json' }
+#
+#     post '/api/v1/road_trip', headers: headers, params: roadtrip_params, as: :json
+#     road_trip_response = JSON.parse(response.body, symbolize_names: true)
+#
+#     road_trip_response_checker(road_trip_response)
+#     expect(response.status).to eq(200)
+#   end
+# end
