566a59d88cb33f7ab4431e0509c85e76d66283a9
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-18 23:03:00 -0700

    Adds road trip response checker

--- spec/response_checkers.rb
+++ spec/response_checkers.rb
@@ -137,4 +137,38 @@ module ResponseCheckers
 
     expect(response[:data][:attributes]).to_not have_key(:password)
   end
+
+  def road_trip_response_checker(response)
+    expect(response).to have_key(:data)
+    expect(response[:data]).to be_a(Hash)
+
+    expect(response[:data]).to have_key(:type)
+    expect(response[:data][:type]).to be_a(String)
+    expect(response[:data][:type]).to eq('road_trip')
+
+    expect(response[:data]).to have_key(:id)
+    expect(response[:data][:id]).to eq(nil)
+
+    expect(response[:data]).to have_key(:attributes)
+    expect(response[:data][:attributes]).to be_a(Hash)
+
+    expect(response[:data][:attributes]).to have_key(:start_city)
+    expect(response[:data][:attributes][:start_city]).to be_a(String)
+
+    expect(response[:data][:attributes]).to have_key(:end_city)
+    expect(response[:data][:attributes][:end_city]).to be_a(String)
+
+    expect(response[:data][:attributes]).to have_key(:travel_time)
+    expect(response[:data][:attributes][:travel_time]).to be_a(String)
+    expect(Time.parse(response[:data][:attributes][:travel_time]).class).to eq(Time)
+
+    expect(response[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(response[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+
+    expect(response[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(response[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Numeric)
+
+    expect(response[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+    expect(response[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
+  end
 end
