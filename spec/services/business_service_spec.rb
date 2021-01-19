0e45b1a739906db0f2bb0f4721d5c7e05efdbd2d
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 11:12:56 -0700

    Adds expectations to business service test

--- spec/services/business_service_spec.rb
+++ spec/services/business_service_spec.rb
@@ -16,9 +16,23 @@ RSpec.describe BusinessService do
       expect(results[:businesses].first[:name]).to be_a(String)
       expect(results[:businesses].first).to have_key(:is_closed)
       expect(results[:businesses].first[:is_closed]).to eq(false)
+      expect(results[:businesses].first).to have_key(:categories)
+      expect(results[:businesses].first[:categories]).to be_an(Array)
+      expect(results[:businesses].first[:categories].first).to be_a(Hash)
+      expect(results[:businesses].first[:categories].first).to have_key(:alias)
+      expect(results[:businesses].first[:categories].first[:alias]).to eq("chinese")
+      expect(results[:businesses].first[:categories].first).to have_key(:title)
+      expect(results[:businesses].first[:categories].first[:title]).to eq("Chinese")
       expect(results[:businesses].first).to have_key(:location)
       expect(results[:businesses].first[:location]).to be_a(Hash)
-      expect(results[:businesses].first[:location]).to have
+      expect(results[:businesses].first[:location]).to have_key(:address1)
+      expect(results[:businesses].first[:location]).to have_key(:address2)
+      expect(results[:businesses].first[:location]).to have_key(:address3)
+      expect(results[:businesses].first[:location]).to have_key(:city)
+      expect(results[:businesses].first[:location]).to have_key(:zip_code)
+      expect(results[:businesses].first[:location]).to have_key(:country)
+      expect(results[:businesses].first[:location]).to have_key(:state)
+      expect(results[:businesses].first[:location]).to have_key(:display_address)
     end
   end
 end
