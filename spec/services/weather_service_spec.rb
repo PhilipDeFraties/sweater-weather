2d364dc0284720916e181522f2a22fc4695f6a3f
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 16:29:16 -0700

    Adds service spec files and cassettes

---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe WeatherService do
+  context 'forecast search' do
+    it "returns appropriate data" do
+      VCR.use_cassette('weatherservice/denver') do
+        coordinates = {lat: 38.2654, lng: -104.6104}
+        results = WeatherService.find_forecast(coordinates)
+
+        expect(results).to have_key(:lat)
+        expect(results[:lat]).to eq(coordinates[:lat])
+        expect(results).to have_key(:lon)
+        expect(results[:lon]).to eq(coordinates[:lng])
+        expect(results).to have_key(:current)
+        expect(results[:current]).to be_a(Hash)
+        expect(results).to have_key(:hourly)
+        expect(results[:hourly]).to be_an(Array)
+        expect(results[:hourly].count).to eq(48)
+        expect(results).to have_key(:daily)
+        expect(results[:daily].count).to eq(8)
+      end
+    end
+  end
+end
