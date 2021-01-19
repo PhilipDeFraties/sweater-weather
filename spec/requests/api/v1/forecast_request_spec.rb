e52b722f49603ca7e224d41db2c0f4ceac8ad9ae
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-18 18:04:40 -0700

    adds sadpath test to forecast request spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe "Forecast" do
     expect(response.status).to eq(200)
     forecast_response_checker(weather_response)
   end
+
+  it "responds with error if query param is empty" do
+    request_params = { location: '' }
+    get '/api/v1/forecast', params: request_params
+
+    error_response = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(response.status).to eq(400)
+    error_response_checker(error_response)
+    expect(error_response[:error]).to eq("missing parameter")
+    expect(error_response[:message]).to eq("Location parameter with value required in search request")
+  end
 end
