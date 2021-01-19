1424311e82f617f56c90fdfa747dbbfcdb286408
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 11:34:21 -0700

    adds munchies poro

---
+++ app/poros/munchies.rb
@@ -0,0 +1,12 @@
+class Munchies
+  attr_reader :destination_city,
+              :travel_time,
+              :forecast,
+              :restaurant
+  def initialize(route_data, weather_data, restaurant_data)
+    @destination_city =
+    @travel_time =
+    @forecast =
+    @restaurant =
+  end
+end
