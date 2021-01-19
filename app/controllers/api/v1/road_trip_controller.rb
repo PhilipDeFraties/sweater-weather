60daf8b5ff01ea1c27f4a3b7c5a0b62351d0e753
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-18 23:04:29 -0700

    Adds road trip controller

---
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class RoadTripController < ApplicationController
+      before_action :verify_api_key
+      def create
+        road_trip = RoadTripFacade.plan_road_trip(road_trip_params)
+        render json: RoadTripSerializer.new(road_trip)
+      end
+
+      private
+
+      def road_trip_params
+        JSON.parse(request.body.read, symbolize_names: true)
+      end
+    end
+  end
+end
