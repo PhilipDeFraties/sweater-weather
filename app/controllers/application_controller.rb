580e0684cfe49966971ef34466634cbc425d17db
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-18 23:04:47 -0700

    adds verify_api_key method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  def verify_api_key
+    unless User.find_by(api_key: "#{road_trip_params[:api_key]}")
+      render json: { message: 'Valid API key required', error: 'Unauthorized' },
+               status: 401
+    end
+  end
 end
