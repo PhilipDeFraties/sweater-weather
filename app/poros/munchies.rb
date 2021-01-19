56af2de12bd445f0a463147e0f4df1792fbe3ea8
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 11:59:12 -0700

    edits munchies poro

--- app/poros/munchies.rb
+++ app/poros/munchies.rb
@@ -4,9 +4,15 @@ class Munchies
               :forecast,
               :restaurant
   def initialize(route_data, weather_data, restaurant_data)
-    @destination_city =
+    @destination_city = format_city(route[:locations][1])
     @travel_time =
-    @forecast =
-    @restaurant =
+    # @forecast =
+    # @restaurant =
+  end
+
+  def format_city
+    city = route[:adminArea5]
+    state = route[:adminArea3]
+    "#{city}, #{state}"
   end
 end
