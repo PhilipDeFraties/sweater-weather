0fffff6f34fd52823ed5e6a1088364fdbe0d22fc
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 11:07:05 -0700

    edits info passed into service method in business service test

--- spec/services/business_service_spec.rb
+++ spec/services/business_service_spec.rb
@@ -3,14 +3,22 @@ require 'rails_helper'
 RSpec.describe BusinessService do
   context 'food search' do
     it "returns appropriate data" do
-      query_params = {
-        start: "denver,co",
-        end: "pueblo,co",
-        food: "chinese"
-      }
-      results = BusinessService.find_food(query_params)
+      type = 'chinese'
+      location = 'pueblo,co'
+      time = Time.now.to_i
+      results = BusinessService.find_food(type, location, time)
 
-      binding.pry
+      expect(results).to be_a(Hash)
+      expect(results).to have_key(:businesses)
+      expect(results[:businesses]).to be_an(Array)
+      expect(results[:businesses].first).to be_a(Hash)
+      expect(results[:businesses].first).to have_key(:name)
+      expect(results[:businesses].first[:name]).to be_a(String)
+      expect(results[:businesses].first).to have_key(:is_closed)
+      expect(results[:businesses].first[:is_closed]).to eq(false)
+      expect(results[:businesses].first).to have_key(:location)
+      expect(results[:businesses].first[:location]).to be_a(Hash)
+      expect(results[:businesses].first[:location]).to have
     end
   end
 end
