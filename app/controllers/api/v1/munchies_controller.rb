187295ddb5f08f1a1854603a0e56d05fdcc5ad96
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 09:51:52 -0700

    Adds logic to search action and params in munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,7 +2,14 @@ module Api
   module V1
     class MunchiesController < ApplicationController
       def search
-        
+        munchies = MunchiesFacade.get_food_and_forecast(munchies_params)
+        render json: MunchiesSerializer.new(munchies)
+      end
+
+      private
+
+      def munchies_params
+        params.permit(:start, :end, :food)
       end
     end
   end
