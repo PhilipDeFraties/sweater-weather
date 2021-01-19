9785d6858144ca48d1aba622f3ff0f03f62032c1
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 09:28:37 -0700

    xits tests instead of commenting out

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,26 +1,26 @@
-# require 'rails_helper'
-#
-# RSpec.describe "Forecast" do
-#   it "returns the weather forecast for a city" do
-#     forecast_params = { location: 'denver, co' }
-#     headers = { 'CONTENT_TYPE' => 'application/json' }
-#     get '/api/v1/forecast', headers: headers, params: forecast_params
-#
-#     weather_response = JSON.parse(response.body, symbolize_names: true)
-#
-#     expect(response.status).to eq(200)
-#     forecast_response_checker(weather_response)
-#   end
-#
-#   it "responds with error if query param is empty" do
-#     request_params = { location: '' }
-#     get '/api/v1/forecast', params: request_params
-#
-#     error_response = JSON.parse(response.body, symbolize_names: true)
-#
-#     expect(response.status).to eq(400)
-#     error_response_checker(error_response)
-#     expect(error_response[:error]).to eq("missing parameter")
-#     expect(error_response[:message]).to eq("Location parameter with value required in search request")
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "Forecast" do
+  xit "returns the weather forecast for a city" do
+    forecast_params = { location: 'denver, co' }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+    get '/api/v1/forecast', headers: headers, params: forecast_params
+
+    weather_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(200)
+    forecast_response_checker(weather_response)
+  end
+
+  xit "responds with error if query param is empty" do
+    request_params = { location: '' }
+    get '/api/v1/forecast', params: request_params
+
+    error_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(400)
+    error_response_checker(error_response)
+    expect(error_response[:error]).to eq("missing parameter")
+    expect(error_response[:message]).to eq("Location parameter with value required in search request")
+  end
+end
