deb6d39d29319c296d0f998f77a05b8154d037ed
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-18 17:36:33 -0700

    adds image service with get_image and conn methods

---
+++ app/services/image_service.rb
@@ -0,0 +1,14 @@
+class ImageService
+  def self.get_image(location)
+    response = conn.get('/search/photos?') do |faraday|
+      faraday.params['query'] = location
+      faraday.params['per_page'] = 1
+      faraday.params['client_id'] = ENV['UNSPLASH_API_KEY']
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def self.conn
+    Faraday.new(url: 'https://api.unsplash.com')
+  end
+end
