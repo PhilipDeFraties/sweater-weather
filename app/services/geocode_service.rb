314ccf57e08c68f1a397328b960bc9290e899ec8
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-18 23:04:02 -0700

    Adds get_route method for geocode service

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -12,4 +12,13 @@ class GeocodeService
   def self.conn
     Faraday.new(url: 'https://www.mapquestapi.com')
   end
+
+  def self.get_route(start_loc, end_loc)
+    response = conn.get('/directions/v2/route?') do |req|
+      req.params['from'] = "#{start_loc}"
+      req.params['to'] = "#{end_loc}"
+      req.params['key'] = ENV['MAPQUEST_API_KEY']
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
