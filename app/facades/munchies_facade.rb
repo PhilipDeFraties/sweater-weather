ef986bba23e16e1e645af967d17d92d69e382a2f
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 11:34:12 -0700

    Edits arguments passed to Business service in munchies facade

--- app/facades/munchies_facade.rb
+++ app/facades/munchies_facade.rb
@@ -2,7 +2,8 @@ class MunchiesFacade
   def self.get_food_and_forecast(query_data)
     route_data = GeocodeService.get_route(query_data[:start], query_data[:end])
     weather_data = WeatherService.find_forecast(route_data[:route][:locations][1][:latLng])
-    restaurant_data = BusinessService.find_food(query_data)
+    restaurant_data = BusinessService.find_food(query_data[:food], query_data[:end], route_data[:route][:realTime])
+    binding.pry
     Munchies.new(route_data, weather_data, restaurant_data)
   end
 end
