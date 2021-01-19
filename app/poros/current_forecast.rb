61eb82935717cc6b37138c01e427df2c5e23f52f
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-18 17:21:00 -0700

    Adds current_forecast poro

---
+++ app/poros/current_forecast.rb
@@ -0,0 +1,25 @@
+class CurrentForecast
+  attr_reader :datetime,
+              :sunrise,
+              :sunset,
+              :temperature,
+              :feels_like,
+              :humidity,
+              :uvi ,
+              :visibility,
+              :conditions,
+              :icon
+
+  def initialize(weather_data)
+    @datetime = Time.at(weather_data[:dt])
+    @sunrise = Time.at(weather_data[:sunrise])
+    @sunset = Time.at(weather_data[:sunset])
+    @temperature = weather_data[:temp]
+    @feels_like = weather_data[:feels_like]
+    @humidity = weather_data[:humidity]
+    @uvi = weather_data[:uvi]
+    @visibility = weather_data[:visibility]
+    @conditions = weather_data[:weather][0][:description]
+    @icon = weather_data[:weather][0][:icon]
+  end
+end
