51fa42542c61face3bbd66bef5d51e5262dce4db
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 19:07:10 -0700

    adds test for image request sad path

--- spec/requests/api/v1/image_request_spec.rb
+++ spec/requests/api/v1/image_request_spec.rb
@@ -12,4 +12,16 @@ describe 'Image Request' do
       expect(response.status).to eq(200)
     end
   end
+
+  it "responds with 'no results in url field if no image found" do
+    VCR.use_cassette('image/empty') do
+      image_params = { location: '~' }
+      get '/api/v1/backgrounds', params: image_params
+
+      image = JSON.parse(response.body, symbolize_names: true)
+      image_response_checker(image)
+      expect(response.status).to eq(200)
+      expect(image[:data][:attributes][:image][:image_url]).to eq('no results')
+    end
+  end
 end
