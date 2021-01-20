6fbfb0c3851fdb99ce3bb567480fc81ff0ac7a3f
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 18:38:13 -0700

    adds conditional logic to road trip facade to filter impossible routes

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,7 +1,11 @@
 class RoadTripFacade
   def self.plan_road_trip(query_data)
     route_data = GeocodeService.get_route(query_data[:origin], query_data[:destination])
-    forecast = WeatherService.find_forecast(route_data[:route][:locations][1][:latLng])
-    RoadTrip.new(route_data, forecast)
+    unless route_data[:info][:statuscode] != 0
+      forecast = WeatherService.find_forecast(route_data[:route][:locations][1][:latLng])
+      RoadTrip.new(route_data, forecast)
+    else
+      ErrorTrip.new(query_data)
+    end
   end
 end
