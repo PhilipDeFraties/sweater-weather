5a406728c07440867d73fa7f5136313bdf879cca
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-20 10:18:54 -0700

    Adds parse forecast and format city methods to foodie class, adds attrs

--- app/poros/foodie.rb
+++ app/poros/foodie.rb
@@ -5,16 +5,29 @@ class Foodie
               :restaurant
 
   def initialize(route_data, weather_data, restaurant_data)
-    binding.pry
     @end_location =  format_city(route_data[:route][:locations][1])
-    # @travel_time =
-    # @forecast =
-    # @restaurant =
+    @travel_time = route_data[:route][:formattedTime]
+    @forecast = parse_forecast(weather_data, route_data[:route][:realTime])
+    binding.pry
+    @restaurant = { name: restaurant_data[:name],
+       address: restaurant_data[:location][:address] }
   end
 
-  def format_city
-    city = route_data[:adminArea5]
-    state = route_data[:adminArea3]
+  def format_city(location)
+    city = location[:adminArea5]
+    state = location[:adminArea3]
     "#{city}, #{state}"
+  end
+
+  def parse_forecast(forecast, travel_time)
+    arrival_time = Time.now.to_time.to_i + travel_time
+    forecast[:hourly].each do |forecast|
+      if forecast[:dt] < arrival_time && (forecast[:dt] + 3600) > arrival_time
+        return  {
+            summary: forecast[:weather][0][:description],
+            temperature: forecast[:temp]
+          }
+      end
+    end
   end
 end
