6885e26397a3f66cf42540e780be380cdeb31903
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-19 18:36:35 -0700

    Adds sad path test for impossible route in roadtrip request spec, adds before each for user

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 describe 'Roadtrip request' do
-  it "returns appropriate data" do
+  before :each do
+    @user = User.create!(email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "password")
+  end
+
+  it "returns roadtrip info in json if route is possible" do
     VCR.use_cassette('road_trip/denver_pueblo') do
-      user = User.create!(email: "whatever@example.com",
-                          password: "password",
-                          password_confirmation: "password")
       roadtrip_params = {
         "origin": "Denver,CO",
         "destination": "Pueblo,CO",
-        "api_key": "#{user.api_key}"
+        "api_key": "#{@user.api_key}"
       }
 
       headers = { 'CONTENT_TYPE' => 'application/json' }
@@ -18,6 +21,24 @@ describe 'Roadtrip request' do
       road_trip_response = JSON.parse(response.body, symbolize_names: true)
 
       road_trip_response_checker(road_trip_response)
+      expect(response.status).to eq(200)
+    end
+  end
+
+  it "returns empty weather and travel time: impossible if no route" do
+    VCR.use_cassette('road_trip/impossible') do
+      roadtrip_params = {
+        "origin": "Denver,CO",
+        "destination": "London,UK",
+        "api_key": "#{@user.api_key}"
+      }
+
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v1/road_trip', headers: headers, params: roadtrip_params, as: :json
+      error_trip_response = JSON.parse(response.body, symbolize_names: true)
+
+      error_trip_checker(error_trip_response)
       expect(response.status).to eq(200)
     end
   end
