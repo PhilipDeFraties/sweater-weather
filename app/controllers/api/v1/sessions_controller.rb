6e1b43d7ec36f2768b44fdbb89efb988b29298a9
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-21 11:24:26 -0700

    adds private parsed params to accept json request body

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,15 +2,20 @@ module Api
   module V1
     class SessionsController < ApplicationController
       def create
-        login_params = JSON.parse(request.body.read, symbolize_names: true)
         user = User.find_by_email(login_params[:email].downcase)
-        if user && user.authenticate(params[:session][:password])
+        if user && user.authenticate(login_params[:password])
           session[:user_id] = user.id
           render json: UserSerializer.new(user), status: 200
         else
           render json: { message: 'Invalid Credentials', error: 'Failed Login' },
                    status: 401
         end
+      end
+
+      private
+
+      def login_params
+        JSON.parse(request.body.read, symbolize_names: true)
       end
     end
   end
