a9cdaef9e2011b4cfe2bb4c77711898fe4455298
PhilipDeFraties <philipjfry338@gmail.com>
2021-04-02 22:34:47 -0600

    changes forecast poro to accept hash of hashes as initializing argument

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -6,18 +6,8 @@ class Forecast
 
   def initialize(weather_data)
     @id = nil
-    @current_weather = CurrentForecast.new(weather_data[:current])
-    @daily_weather = daily_forecasts(weather_data[:daily][0..4])
-    @hourly_weather = hourly_forecasts(weather_data[:hourly][0..7])
-  end
-
-  private
-
-  def daily_forecasts(daily_data)
-    daily_data.map { |day| DailyForecast.new(day) }
-  end
-
-  def hourly_forecasts(hourly_data)
-    hourly_data.map { |hour| HourlyForecast.new(hour) }
+    @current_weather = weather_data[:current_weather]
+    @daily_weather = weather_data[:daily_weather]
+    @hourly_weather = weather_data[:hourly_weather]
   end
 end
