6056a3da21666fd774c8998218ebbb3230c17a45
PhilipDeFraties <philipjfry338@gmail.com>
2021-04-15 14:07:42 -0600

    refactors get_forecast method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -2,10 +2,9 @@ class ForecastFacade
   def self.get_forecast(location)
     coordinates = GeocodeService.find_coordinates(location)
     weather_data = WeatherService.find_forecast(coordinates)
-    forecasts = {
-    current_weather: CurrentForecast.new(weather_data[:current]),
-    daily_weather: weather_data[:daily][0..4].map { |day| DailyForecast.new(day) },
-    hourly_weather: weather_data[:hourly][0..7].map { |hour| HourlyForecast.new(hour) } }
-    forecast = Forecast.new(forecasts)
+    weather_hash = { current_weather: CurrentForecast.new(weather_data[:current]),
+                     daily_weather: weather_data[:daily][0..4].map { |day| DailyForecast.new(day) },
+                     hourly_weather: weather_data[:hourly][0..7].map { |hour| HourlyForecast.new(hour) } }
+    forecast = Forecast.new(weather_hash)
   end
 end
