81e32ce0539ee0211df36de410c605663656f21e
PhilipDeFraties <philipjfry338@gmail.com>
2021-04-20 17:50:50 -0600

    Edits plan_road_trip method to use if statement instead of unless

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,7 +1,7 @@
 class RoadTripFacade
   def self.plan_road_trip(query_data)
     route_data = GeocodeService.get_route(query_data[:origin], query_data[:destination])
-    unless route_data[:info][:statuscode] != 0
+    if route_data[:info][:statuscode] == 0
       forecast = WeatherService.find_forecast(route_data[:route][:locations][1][:latLng])
       RoadTrip.new(route_data, forecast)
     else
