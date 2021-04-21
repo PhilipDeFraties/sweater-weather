1f205bb1c249aa905948722a285b277a4869bd4f
PhilipDeFraties <philipjfry338@gmail.com>
2021-04-20 18:12:13 -0600

    refactors search action in roadtrip controller to call verify_api_key method with key param as argument

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,8 +1,8 @@
 module Api
   module V1
     class RoadTripController < ApplicationController
-      before_action :verify_api_key
       def search
+        verify_api_key(road_trip_params[:api_key])
         road_trip = RoadTripFacade.plan_road_trip(road_trip_params)
         render json: RoadTripSerializer.new(road_trip)
       end
