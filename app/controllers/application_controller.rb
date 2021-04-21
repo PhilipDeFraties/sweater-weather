ffa5f622dad2d5b3641305236aceef1711c08202
PhilipDeFraties <philipjfry338@gmail.com>
2021-04-20 18:12:43 -0600

    refactors verify_api_key method to take key as argument to be reusable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-  def verify_api_key
-    unless User.find_by(api_key: "#{road_trip_params[:api_key]}")
+  def verify_api_key(api_key)
+    unless User.find_by(api_key: "#{api_key}")
       render json: { message: 'Valid API key required', error: 'Unauthorized' },
                status: 401
     end
