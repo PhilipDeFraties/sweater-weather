cdf78b8e908227f3e2ed4a6858dd977b942f003f
PhilipDeFraties <philipjfry338@gmail.com>
2021-04-21 22:00:33 -0600

    updates services specs to use simpler vcr syntax

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -2,24 +2,22 @@ require 'rails_helper'
 
 RSpec.describe ImageService do
   context 'image search' do
-    it "returns appropriate data" do
-      VCR.use_cassette('image/denver3') do
-        image = ImageService.get_image('denver,co')
+    scenario "returns appropriate data", :vcr do
+      image = ImageService.get_image('denver,co')
 
-        expect(image).to be_a(Hash)
-        expect(image).to have_key(:total)
-        expect(image[:total]).to be_an(Integer)
-        expect(image).to have_key(:results)
-        expect(image[:results]).to be_an(Array)
-        expect(image[:results][0]).to have_key(:urls)
-        expect(image[:results][0][:urls]).to be_a(Hash)
-        expect(image[:results][0][:urls]).to have_key(:regular)
-        expect(image[:results][0][:urls][:regular]).to be_a(String)
-        expect(image[:results][0]).to have_key(:user)
-        expect(image[:results][0][:user]).to be_a(Hash)
-        expect(image[:results][0][:user]).to have_key(:username)
-        expect(image[:results][0][:user][:username]).to be_a(String)
-      end
+      expect(image).to be_a(Hash)
+      expect(image).to have_key(:total)
+      expect(image[:total]).to be_an(Integer)
+      expect(image).to have_key(:results)
+      expect(image[:results]).to be_an(Array)
+      expect(image[:results][0]).to have_key(:urls)
+      expect(image[:results][0][:urls]).to be_a(Hash)
+      expect(image[:results][0][:urls]).to have_key(:regular)
+      expect(image[:results][0][:urls][:regular]).to be_a(String)
+      expect(image[:results][0]).to have_key(:user)
+      expect(image[:results][0][:user]).to be_a(Hash)
+      expect(image[:results][0][:user]).to have_key(:username)
+      expect(image[:results][0][:user][:username]).to be_a(String)
     end
   end
 end
