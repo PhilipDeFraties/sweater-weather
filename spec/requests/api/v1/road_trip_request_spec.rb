52093ebfdb6edbf7a2c131e422b787d8b3bcce50
PhilipDeFraties <philipjfry338@gmail.com>
2021-04-21 21:48:19 -0600

    Refactors road_trip_request_spec.rb to use simpler vcr syntax, cleans up spacing

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Roadtrip request' do
+RSpec.describe 'Roadtrip request' do
   before :each do
     @user = User.create!(email: "whatever@example.com",
       password: "password",
@@ -8,33 +8,27 @@ describe 'Roadtrip request' do
   end
 
   feature "roadtrip" do
-    it "returns roadtrip info in json if route is possible" do
-      VCR.use_cassette('road_trip/denver_pueblo') do
-        roadtrip_params = {
-          "origin": "Denver,CO",
-          "destination": "Pueblo,CO",
-          "api_key": "#{@user.api_key}"
-        }
+    scenario "returns roadtrip info in json if route is possible", :vcr do
+      roadtrip_params = {
+        "origin": "Denver,CO",
+        "destination": "Pueblo,CO",
+        "api_key": "#{@user.api_key}"
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json' }
 
-        headers = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/road_trip', headers: headers, params: roadtrip_params, as: :json
+      road_trip_response = JSON.parse(response.body, symbolize_names: true)
 
-        post '/api/v1/road_trip', headers: headers, params: roadtrip_params, as: :json
-        road_trip_response = JSON.parse(response.body, symbolize_names: true)
-
-        road_trip_response_checker(road_trip_response)
-        expect(response.status).to eq(200)
-      end
+      road_trip_response_checker(road_trip_response)
+      expect(response.status).to eq(200)
     end
-  end
 
-  scenario "returns empty weather and travel time: impossible if no route", :vcr do
-    # VCR.use_cassette('road_trip/impossible') do
+    scenario "returns empty weather and travel time: impossible if no route", :vcr do
       roadtrip_params = {
         "origin": "Denver,CO",
         "destination": "London,UK",
         "api_key": "#{@user.api_key}"
       }
-
       headers = { 'CONTENT_TYPE' => 'application/json' }
 
       post '/api/v1/road_trip', headers: headers, params: roadtrip_params, as: :json
@@ -42,5 +36,6 @@ describe 'Roadtrip request' do
 
       error_trip_checker(error_trip_response)
       expect(response.status).to eq(200)
+    end
   end
 end
